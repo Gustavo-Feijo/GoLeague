9f2b52b201870142e9b642264717ca0682a2a582
Gustavo-Feijo <gustavofeijo.dev@gmail.com>
2025-01-20 12:46:55 -0300

    ADD: Database configuration, Gorm and simple test connection.

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"goleague/pkg/config"
 	"goleague/pkg/redis"
+	"log"
 	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	config.LoadEnv()
 	client := redis.GetClient()
 	client.Set(context.Background(), "teste", "batata", time.Hour)
+	fmt.Println(config.Database.Port)
+	_, err := gorm.Open(postgres.Open(config.Database.URL), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect %v", err)
+	}
+	fmt.Println("Connected")
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,17 +1,25 @@
 package config
 
 import (
+	"fmt"
 	"os"
 )
 
 // Redis configuration struct.
 type RedisConfiguration struct {
 	Host     string
-	Port     string
 	Password string
+	Port     string
 }
 
-type DatabaseConfiguration struct{}
+type DatabaseConfiguration struct {
+	Database string
+	Host     string
+	Password string
+	Port     string
+	User     string
+	URL      string
+}
 
 var (
 	Redis    RedisConfiguration
@@ -22,6 +30,14 @@ var (
 func LoadEnv() {
 	// Load the Redis configuration.
 	Redis.Host = os.Getenv("REDIS_HOST")
-	Redis.Port = os.Getenv("REDIS_PORT")
 	Redis.Password = os.Getenv("REDIS_PASSWORD")
+	Redis.Port = os.Getenv("REDIS_PORT")
+
+	// Load the database configuration.
+	Database.Database = os.Getenv("POSTGRES_DATABASE")
+	Database.Host = os.Getenv("POSTGRES_HOST")
+	Database.Password = os.Getenv("POSTGRES_PASSWORD")
+	Database.Port = os.Getenv("POSTGRES_PORT")
+	Database.User = os.Getenv("POSTGRES_USER")
+	Database.URL = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC", Database.Host, Database.User, Database.Password, Database.Database, Database.Port)
 }
