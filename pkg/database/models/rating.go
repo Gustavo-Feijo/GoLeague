bf3729c780ece259c4a9b67355b9f53bd5d5b920
Gustavo-Feijo <gustavofeijo.dev@gmail.com>
2025-04-30 00:08:40 -0300

    Improve indexing.

--- pkg/database/models/rating.go
+++ pkg/database/models/rating.go
@@ -10,16 +10,16 @@ type RatingEntry struct {
 	ID uint `gorm:"primaryKey"`
 
 	// Reference to the player that has the rating.
-	PlayerId uint       `gorm:"primaryKey;autoIncrement:false"`
+	PlayerId uint       `gorm:"index:index_player_time,priority:1"`
 	Player   PlayerInfo `gorm:"PlayerId"`
 
 	Queue        string `gorm:"type:queue_type"`
 	Tier         string `gorm:"type:tier_type"`
 	Rank         string `gorm:"type:rank_type"`
-	NumericScore int    `gorm:"index"`
+	NumericScore int
 	LeaguePoints int
 	Wins         int
 	Losses       int
 	Region       regions.SubRegion `gorm:"type:varchar(5)"`
-	FetchTime    time.Time         `gorm:"autoCreateTime"`
+	FetchTime    time.Time         `gorm:"autoCreateTime;index:index_player_time,priority:2"`
 }
